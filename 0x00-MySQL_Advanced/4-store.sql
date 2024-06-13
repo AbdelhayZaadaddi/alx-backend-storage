4f5260bbf330a6be6f8159872d0ad0aad8e38241
Abdelhay zaadaddi <abdezaadaddi06@gmail.com>
2024-06-13 10:02:22 +0100

    4-store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,5 @@
+-- Creates  a trigger that descreases the quantity
+-- Of an item after adding a new order
+
+CREATE TRIGGER decrease_items_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
