8e2da6fbb3d90dd5d7b0abcafb4b8145b9ae3f9c
Abdelhay zaadaddi <abdezaadaddi06@gmail.com>
2024-06-13 10:36:28 +0100

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,24 @@
--- Procedure: ComputeAverageScoreForUser
--- Description: Updates the average score for a specified user in the users table based on the scores in the corrections table
-
-DELIMIER $$
+-- Creates a stored procedure ComputeAverageScoreForUser that
+-- computes and stores the average score for a student.
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    UPDATE users
-    SET
-    average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
-    WHERE id = user_id;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
 END $$
-DELIMIER;
+DELIMITER ;
